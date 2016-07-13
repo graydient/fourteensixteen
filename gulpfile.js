1083d0116586675f7fe20c1bfcfd93412991157a
Miles <mileskoller@gmail.com>
2016-07-13 10:00:56 -0500

    build system complete

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,34 @@ var browserSync = require('browser-sync').create();
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var cache = require('gulp-cache');
+var runSequence = require('run-sequence');
+
+
+// Gulp Dev Task
+gulp.task('default', function (callback) {
+  runSequence(['sass','browserSync', 'watch'],
+    callback
+  )
+})
 
 // Gulp Watch task
 gulp.task('watch', ['browserSync', 'sass'], function(){
   gulp.watch('app/scss/**/*.scss', ['sass']); 
   gulp.watch('app/*.html', browserSync.reload); 
   gulp.watch('app/js/**/*.js', browserSync.reload);
+})
+
+// Gulp Build Task
+gulp.task('build', function (callback) {
+  runSequence('clean:dist', 
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+  )
 })
 
 // Gulp Sass task
@@ -38,5 +60,37 @@ gulp.task('useref', function(){
     .pipe(useref())
     // Minifies only if it's a JavaScript file
     .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'))
 });
+
+// Gulp Images Task
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin())
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+  })))
+  .pipe(gulp.dest('dist/images'))
+});
+
+// Gulp Fonts Task
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
+
+// Gulp Delete Task
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+// Gulp Clear Cache
+gulp.task('clear', function (callback) {
+  return cache.clearAll(callback)
+})
+
+
+
